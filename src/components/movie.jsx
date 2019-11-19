930d50e3dffd1cd7fad2279543a206f0e6943a3c
Marzena <marzena.szopinska090@gmail.com>
2019-11-19 11:59:02 +0100

    added functionality to send user back to home page after clicking on a button inside the movie component

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
-const Movie = () => {
-    return (
-        <h1> Movie From - id  </h1>
-    );
+class Movie extends Component {
+
+    handleSave = () => {
+        // go to home page
+        this.props.history.replace('/movies');
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> Movie From - id  </h1>
+                <button className='btn btn-small' onClick={this.handleSave}>Save</button>
+            </div>
+        );
+    }
 }
 
 export default Movie;
