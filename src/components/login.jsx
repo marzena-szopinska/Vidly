a831ec9448621190fa0084e47e4974a2394fa1b4
Marzena <marzena.szopinska090@gmail.com>
2019-11-20 11:39:21 +0100

    added basic input validation

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,12 +3,34 @@ import Input from './input';
 
 class Login extends React.Component {
     state = {
-        account: { username: '', password: '' }
+        account: { username: '', password: '' },
+        errors: {} // all errors will be stored here
     };
+
+    validate = () => {
+        const errors = {};
+        if (this.state.account.username.trim() === '')
+            // add a key
+            errors.username = 'Username required!';
+
+        if (this.state.account.password.trim() === '')
+            // add a key
+            errors.password = 'Password required!';
+        // if there are any errors return errors otherwise return nothing (null)
+        return Object.keys(errors).length === 0 ? null : errors;
+    }
 
     handleSubmit = (e) => {
         // prevent a full page reload
         e.preventDefault();
+        // check for errors
+        const errors = this.validate();
+        this.setState({ errors });
+        console.log(errors);
+        if (errors) return;
+
+        // Call the server
+        console.log('Submitted');
     }
 
     handleChange = e => {
