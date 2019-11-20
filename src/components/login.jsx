5f9fed7d383be6bd4ac5748b89022944ef53a17f
Marzena <marzena.szopinska090@gmail.com>
2019-11-20 10:16:11 +0100

    changed regular inputs into controlled elements

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,26 +1,50 @@
 import React from 'react';
 
-const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-            <br />
-            <form>
-                <div className='form-group'>
-                    <label htmlFor='username'>Username:</label>
-                    <input id='username' type='text' className='form-control' />
-                </div>
-                <div className='form-group'>
-                    <label htmlFor='password'>Password:</label>
-                    <input id='password' type='text' className='form-control' />
-                </div>
-                <button className='btn btn-primary'>Login</button>
+class Login extends React.Component {
+    state = {
+        account: { username: '', password: '' }
+    };
 
-            </form>
+    handleSubmit = (e) => {
+        // prevent a full page reload
+        e.preventDefault();
+    }
 
-        </div>
+    handleChange = e => {
+        const account = { ...this.state.account };
+        account[e.currentTarget.name] = e.currentTarget.value;
+        this.setState({ account });
+        // test
+        console.log(this.state.account.username);
+        console.log(this.state.account.password);
+    }
 
-    );
+    render() {
+
+        const { account } = this.state;
+
+        return (
+            <div>
+                <h1>Login</h1>
+                <br />
+                <form onSubmit={this.handleSubmit}>
+                    <div className='form-group'>
+                        <label htmlFor='username'>Username:</label>
+                        <input value={account.username} onChange={this.handleChange} name='username' autoFocus id='username' type='text' className='form-control' />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='password'>Password:</label>
+                        <input value={account.password} onChange={this.handleChange} name='password' id='password' type='text' className='form-control' />
+                    </div>
+                    <button className='btn btn-primary'>Login</button>
+
+                </form>
+
+            </div>
+
+        );
+    }
+
 }
 
 export default Login;
